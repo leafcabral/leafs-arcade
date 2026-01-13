d65c1d7b9c4cb8c407475fc81c111162a3ed5c32
leafcabral <rafaelcabral4100@gmail.com>
2026-01-13 19:47:47 -0300

    improv(fruit_ninja): Reworked MouseTrailComponent to be way more similar to the original Fruit Ninja's blade effect
    Changed Mouse Trail scene to make it more similar to the original game
    
    Limited amount of points in the trail
    
    Made trail fading depend on the total length of the trail and an arbitrary speed factor (with a cap for minimum speed)
    
    Refactored the _process(...) function  to be more compact and use helper function

--- 4-fruit-ninja/common/components/mouse_trail_component.gd
+++ 4-fruit-ninja/common/components/mouse_trail_component.gd
@@ -2,12 +2,14 @@ class_name MouseTrailComponent
 extends Line2D
 
 
-@export_range(0.0, 2.5, 0.01, "or_greater", "suffix:s") var fade_start_delay := 0.1
-@export_range(0.0, 2.5, 0.01, "or_greater", "suffix:s") var point_fade_delay := 0.0
 @export_range(0.0, 10.0, 0.1, "suffix:px") var point_spacing := 5.0
+@export_range(2, 100, 1, "or_greater") var max_points := 32
+@export_group("Vanishing Effect")
+@export_range(1, 20, 0.1) var speed_factor := 8.0
+@export_range(1, 1000, 0.1) var minimum_speed := 100.0
+@export_range(0, 1, 0.01, "or_greater", "suffix:s") var start_delay := 0.15
 
 var time_since_trail_start := 0.0
-var time_since_last_point := 0.0
 
 
 func _process(delta: float) -> void:
@@ -15,31 +17,67 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("slice"):
 		clear_points()
+		add_point(mouse_position)
+		
 		time_since_trail_start = 0.0
-		
-	var number_of_points := get_point_count()
-	if Input.is_action_pressed("slice"):
-		var can_place_point := true
-		
-		if number_of_points:
-			var distance_to_last := mouse_position.distance_to(points[-1])
-			if distance_to_last < point_spacing:
-				can_place_point = false
-		else:
-			time_since_trail_start = 0.0
-		
-		if can_place_point:
-			add_point(mouse_position)
-		
-		if number_of_points == 2:
-			add_point((points[0] + points[1]) / 2)
+	elif Input.is_action_pressed("slice"):
+		handle_mouse_movement(mouse_position)
 	
-	if number_of_points >= 2:
-		if time_since_trail_start >= fade_start_delay:
-			if time_since_last_point >= point_fade_delay:
-				remove_point(0)
-				time_since_last_point = 0.0
-			else:
-				time_since_last_point += delta
+	if time_since_trail_start >= start_delay:
+		remove_trail_points(delta)
+	else:
+		time_since_trail_start += delta
+
+
+func handle_mouse_movement(mouse_position: Vector2) -> void:
+	var number_of_points := get_point_count()
+	
+	if number_of_points > 1:
+		var distance_to_last := mouse_position.distance_to(points[-1])
+		if distance_to_last < point_spacing:
+			return
+	else:
+		time_since_trail_start = 0.0
+	
+	add_point(mouse_position)
+	if number_of_points + 1 > max_points:
+		remove_point(0)
+	# Workaround for width_curve "glitch" where the line won't show
+	# if there are only two points in the line
+	if get_point_count() == 2:
+		add_point((points[0] + points[1]) / 2)
+
+
+func remove_trail_points(delta: float) -> void:
+	var number_of_points := get_point_count()
+	if number_of_points < 2:
+		if number_of_points == 1:
+			remove_point(0)
+		return
+	
+	var distance_to_remove := get_relative_vanishing_speed() * delta
+	while distance_to_remove > 0 and get_point_count() >= 2:
+		var a := points[0]
+		var b := points[1]
+		var segment_length := a.distance_to(b)
+		
+		if segment_length  <= distance_to_remove:
+			remove_point(0)
+			distance_to_remove -= segment_length
 		else:
-			time_since_trail_start += delta
+			var new_pos := a.move_toward(b,
+				distance_to_remove
+			)
+			set_point_position(0, new_pos)
+			distance_to_remove = 0
+			return
+
+
+func get_relative_vanishing_speed() -> float:
+	var number_of_points := get_point_count()
+	
+	var total_trail_length := 0.0
+	for i in range(number_of_points - 1):
+		total_trail_length += points[i].distance_to(points[i + 1])
+	
+	return maxf(total_trail_length * speed_factor, minimum_speed)
