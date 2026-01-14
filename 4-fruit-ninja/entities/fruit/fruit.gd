28d3c0a393c2e2ffa6df76e170d6ad3e75333217
leafcabral <rafaelcabral4100@gmail.com>
2026-01-14 16:57:36 -0300

    feat(fruit_ninja): Added fruits that will launch themselves without leaving the screen upwards or sideways
    Created a Fruit scene based on RigidBody2D (layer 2 for fruits) with GradientTexture2D as placeholder for sprite and RectangleShape2D as placeholder for collision
    
    The RigidBody2D in fruit.tscn uses an Angular Damp of -1 so its angular velocity stay constant
    
    Added fruit.gd to the Fruit scene that randomizes its root RigitBody2D Angular Velocity and randomily sets its Linear Velocity, so that it never leaves the screen (except for falling down) and tries to go to the other side of the screen

---
+++ 4-fruit-ninja/entities/fruit/fruit.gd
@@ -0,0 +1,32 @@
+class_name Fruit
+extends RigidBody2D
+
+
+const AVG_SPEED := 300.0
+const MAX_ANGLE_DELTA := PI / 3
+
+@onready var viewport_size := get_viewport_rect().size
+
+
+func _ready() -> void:
+	launch()
+
+
+func launch() -> void:
+	angular_velocity = randf_range(PI / 2, PI)
+	
+	var half_sprite_size: Vector2 = $Sprite2D.get_rect().size / 2
+	var x_position_ratio := remap(
+		global_position.x,
+		half_sprite_size.x, viewport_size.x - half_sprite_size.x,
+		-1, 1
+	)
+	
+	var x_move_half_screen := viewport_size.x / 4
+	var x_move_full_screen := viewport_size.x / 2
+	linear_velocity.x = (
+		-x_position_ratio * randf_range(x_move_half_screen, x_move_full_screen)
+		if abs(x_position_ratio) >= 0.2 else
+		[-1, 1].pick_random() * randf_range(x_move_half_screen, x_move_half_screen * 1.1)
+	)
+	linear_velocity.y = -viewport_size.y * randf_range(1.5, 1.8)
