b0af77a5f09da5a2bb2de1d7cac35f5fc70debc8
leafcabral <rafaelcabral4100@gmail.com>
2026-01-16 16:12:49 -0300

    chore(fruit_ninja): Added documentation comments to MouseTrailComponent
    Added documentation comments to the entire MouseTrailComponent script.
    
    Made MouseTrailComponent methods private because they shouldn't be called by other parties.
    
    Added a variable to MouseTrailComponent that defines the Input action to be checked for creating the mouse trail.
    
    Changed some @export_range(...) parameters in MouseTrailComponent to make it more universal.
    
    Changed Player scene MouseTrailComponent name to "Blade" and made its CollisionObject2D reference be its child with name "CollisionArea"

--- 4-fruit-ninja/common/components/mouse_trail_component.gd
+++ 4-fruit-ninja/common/components/mouse_trail_component.gd
@@ -1,46 +1,78 @@
 class_name MouseTrailComponent
 extends Line2D
+## Component that creates a trail that follows the mouse.
+## 
+## [b]MouseTrailComponent[/b] is a Line2D that creates points that follows the
+## mouse when [member input_action] is pressed.
+## [br][br]
+## If the trail should act as a [CollisionObject2D], [member collision_enabled]
+## should be set to [code]true[/code] and [member collision_node] should
+## reference a [CollisionObject2D] that will contain the [CollisionShape2D]s of
+## the trail.
 
 
+## Name of the [InputEventAction] that will be checked every physical frame to
+## create the trail.
+@export var input_action := &""
+## Maximum number of points the trail will have. If [member Line2D.points]
+## surpass [member max_points], the point in the tail of the trail will be
+## removed.
 @export_range(2, 100, 1, "or_greater") var max_points := 32
+## Minimum distance in pixels between two consecutive points.
 @export_range(0.0, 50.0, 0.01, "suffix:px") var min_point_spacing := 5.0
 
 @export_group("Vanishing Effect")
-@export_range(1, 20, 0.1) var speed_factor := 8.0
-@export_range(1, 1000, 0.1) var minimum_speed := 100.0
-@export_range(0, 1, 0.01, "or_greater", "suffix:s") var start_delay := 0.15
+## Influence how fast the trail will vanish. Values bigger than 20 will usually
+## make the trail immediately vanish after [member start_delay] reaches 0. A
+## value close or equal to 0 may not work if [member minimum_speed] is set
+@export_range(0, 20, 0.1, "or_greater") var speed_factor := 8.0
+## The minimum speed in [code]pixels/seconds[/code] the trail will vanish every
+## physical frame after [member start_delay] reaches 0.
+@export_range(0, 1000, 0.1, "or_greater", "suffix:px/s") var minimum_speed := 100.0
+## Seconds after [member input_action] is pressed for the trail to start
+## vanishing. Won't be totally acurate as it uses [param delta] in
+## [method Node._physics_process].
+@export_range(0, 10, 0.01, "or_greater", "suffix:s") var start_delay := 0.15
 
 @export_group("Collision", "collision_")
+## Determines if the trail will create [CollisionShape2D]s as a child of
+## [member collision_node]. If [member collision_node] isn't set, no 
+## [CollisionShape2D] will be created
 @export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var collision_enabled := false
+## The [CollisionObject2D] that will contain the collision shapes for the
+## trail. If [member collision_enabled] isn't set to [code]true[/code], no 
+## [CollisionShape2D] will be created
 @export var collision_node: CollisionObject2D
-@export_range(0, 100, 1) var collision_max_lines := 4
+## Maximum amount of [SegmentShape2D] created, starting the head of the trail
+@export_range(0, 100, 1, "or_greater") var collision_max_lines := 4
 
 @export_group("Show Points", "points_")
+## If [code]true[/code], draws circles at each point in the trail.
 @export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var points_enabled := false
+## Color of the points when [member points_enabled] is [code]true[/code].
 @export var points_color := Color.RED
+## Radius of the points when [member points_enabled] is [code]true[/code].
 @export_range(0, 10, 0.1, "suffix:px") var points_radius := 5.0
 
 
-var time_since_trail_start := 0.0
-
-@onready var collision_objects_2d := find_children("*", "CollisionObject2D", false)
+var _time_since_trail_start := 0.0
 
 
 func _physics_process(delta: float) -> void:
 	var mouse_position := get_local_mouse_position()
 	
-	if Input.is_action_just_pressed("slice"):
+	if Input.is_action_just_pressed(input_action):
 		clear_points()
-	elif Input.is_action_pressed("slice"):
-		handle_mouse_movement(mouse_position)
+	elif Input.is_action_pressed(input_action):
+		_handle_mouse_movement(mouse_position)
 	
-	if time_since_trail_start >= start_delay:
-		remove_trail_points(delta)
+	if _time_since_trail_start >= start_delay:
+		_remove_trail_points(delta)
 	else:
-		time_since_trail_start += delta
+		_time_since_trail_start += delta
 	
 	if collision_enabled and collision_node:
-		create_collision_shapes()
+		_create_collision_shapes()
 
 
 func _draw() -> void:
@@ -49,7 +81,7 @@ func _draw() -> void:
 			draw_circle(i, points_radius, points_color)
 
 
-func handle_mouse_movement(mouse_position: Vector2) -> void:
+func _handle_mouse_movement(mouse_position: Vector2) -> void:
 	var number_of_points := get_point_count()
 	
 	if number_of_points >= 1:
@@ -57,7 +89,7 @@ func handle_mouse_movement(mouse_position: Vector2) -> void:
 		if distance_to_last < min_point_spacing:
 			return
 	else:
-		time_since_trail_start = 0.0
+		_time_since_trail_start = 0.0
 	
 	add_point(mouse_position)
 	if number_of_points + 1 > max_points:
@@ -69,14 +101,14 @@ func handle_mouse_movement(mouse_position: Vector2) -> void:
 		add_point((points[0] + points[1]) / 2)
 
 
-func remove_trail_points(delta: float) -> void:
+func _remove_trail_points(delta: float) -> void:
 	var number_of_points := get_point_count()
 	if number_of_points < 2:
 		if number_of_points == 1:
 			remove_point(0)
 		return
 	
-	var distance_to_remove := get_relative_vanishing_speed() * delta
+	var distance_to_remove := _get_relative_vanishing_speed() * delta
 	while distance_to_remove > 0 and get_point_count() >= 2:
 		var a := points[0]
 		var b := points[1]
@@ -94,7 +126,7 @@ func remove_trail_points(delta: float) -> void:
 			return
 
 
-func get_relative_vanishing_speed() -> float:
+func _get_relative_vanishing_speed() -> float:
 	var number_of_points := get_point_count()
 	
 	var total_trail_length := 0.0
@@ -104,12 +136,14 @@ func get_relative_vanishing_speed() -> float:
 	return maxf(total_trail_length * speed_factor, minimum_speed)
 
 
-func create_collision_shapes() -> void:
+func _create_collision_shapes() -> void:
 	for collision_shape in collision_node.get_children():
 		collision_shape.queue_free()
 	
 	if points:
-		var num_points_for_collision: int = min(get_point_count(), collision_max_lines + 1)
+		var num_points_for_collision: int = min(
+			get_point_count(), collision_max_lines + 1
+		)
 		for i in range(-1, -num_points_for_collision, -1):
 			var segment := SegmentShape2D.new()
 			segment.a = points[i]
