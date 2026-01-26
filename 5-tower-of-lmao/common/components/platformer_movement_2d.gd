e4d192e19658d0efda3c45a76bb190c901dbb195
leafcabral <rafaelcabral4100@gmail.com>
2026-01-25 21:31:11 -0300

    refactor(tower_of_lmao): improve readability in PlataformMovement2D with DRY

--- 5-tower-of-lmao/common/components/platformer_movement_2d.gd
+++ 5-tower-of-lmao/common/components/platformer_movement_2d.gd
@@ -3,6 +3,10 @@ class_name PlatformerMovement2D
 extends Node2D
 
 
+const CC_LEFT_NAME := ^"CornerCorrectionLeft"
+const CC_MIDDLE_NAME := ^"CornerCorrectionMiddle"
+const CC_RIGHT_NAME := ^"CornerCorrectionRight"
+
 @export var disabled := false
 @export var input_move_left := &"move_left"
 @export var input_move_right := &"move_right"
@@ -54,6 +58,19 @@ var _corner_correction_right: RayCast2D
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PARENTED:
 		parent = get_parent()
+		update_configuration_warnings()
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := PackedStringArray()
+	
+	if not parent is CharacterBody2D:
+		warnings.append(
+			"PlataformerMovement2D only serves to provide movement for a CharacterBody2D derived node.
+			Please, only use it as a child of CharacterBody2D to make it move."
+		)
+		
+	return warnings
 
 
 func _ready() -> void:
@@ -61,85 +78,33 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		update_configuration_warnings()
-		return
-	if disabled:
+	if Engine.is_editor_hint() or disabled:
 		return
 	
-	_handle_horizontal_movement(delta)
-	_handle_vertical_movement(delta)
+	process_movement(delta)
 	
 	parent.move_and_slide()
+	
 	if parent.velocity.y < 0 and not _corner_correction_middle.is_colliding():
 		_try_corner_correction()
 
 
-func _get_configuration_warnings() -> PackedStringArray:
-	var errors := PackedStringArray()
+func process_movement(delta: float) -> void:
+	_handle_horizontal_movement(delta)
+	_handle_vertical_movement(delta)
 	
-	if not parent is CharacterBody2D:
-		errors.append(
-			"PlataformerMovement2D only serves to provide movement for a CharacterBody2D derived node.
-			Please, only use it as a child of CharacterBody2D to make it move."
-		)
-		
-	return errors
-
-
-func _setup_corner_correction_nodes() -> void:
-	_corner_correction_left = get_node_or_null("CornerCorrectionLeft")
-	_corner_correction_middle = get_node_or_null("CornerCorrectionMiddle")
-	_corner_correction_right = get_node_or_null("CornerCorrectionRight")
-	
-	if not _corner_correction_left:
-		_corner_correction_left = RayCast2D.new()
-		_corner_correction_left.name = "CornerCorrectionLeft"
-		_corner_correction_left.collision_mask = parent.collision_mask
-		add_child(_corner_correction_left)
-	
-	if not _corner_correction_middle:
-		_corner_correction_middle = ShapeCast2D.new()
-		_corner_correction_middle.name = "CornerCorrectionMiddle"
-		_corner_correction_middle.shape = RectangleShape2D.new()
-		_corner_correction_middle.collision_mask = parent.collision_mask
-		add_child(_corner_correction_middle)
-	
-	if not _corner_correction_right:
-		_corner_correction_right = RayCast2D.new()
-		_corner_correction_right.name = "CornerCorrectionRight"
-		_corner_correction_right.collision_mask = parent.collision_mask
-		add_child(_corner_correction_right)
-	
-	_update_corner_correction_properties()
-
-
-func _update_corner_correction_properties() -> void:
-	var half_distance := corner_correction_distance_between / 2
-	
-	if _corner_correction_left:
-		_corner_correction_left.position = corner_correction_offset
-		_corner_correction_left.position.x -= half_distance
-		_corner_correction_left.target_position.y = -corner_correction_ray_length
-	
-	if _corner_correction_right:
-		_corner_correction_right.position = corner_correction_offset
-		_corner_correction_right.position.x += half_distance
-		_corner_correction_right.target_position.y = -corner_correction_ray_length
-	
-	if _corner_correction_middle:
-		_corner_correction_middle.position = corner_correction_offset
-		_corner_correction_middle.position.y -= corner_correction_ray_length / 2
-		_corner_correction_middle.target_position = Vector2.ZERO
-		var _shape := _corner_correction_middle.shape as RectangleShape2D
-		_shape.size.x = corner_correction_distance_between - 2 * corner_correction_amount
-		_shape.size.y = corner_correction_ray_length
+	if jump_buffering > 0:
+		jump_buffering = max(0, jump_buffering - delta)
+	if coyote_jump > 0:
+		coyote_jump = max(0, coyote_jump - delta)
 
 
 func _handle_horizontal_movement(delta: float) -> void:
-	var direction := Input.get_axis(input_move_left, input_move_right)
+	var previous_direction := signf(parent.velocity.x)
+	var direction = Input.get_axis(input_move_left, input_move_right)
 	var delta_speed := max_speed * delta
-	if signf(direction) == signf(parent.velocity.x):
+	
+	if direction == previous_direction:
 		parent.velocity.x = move_toward(parent.velocity.x, direction * max_speed, delta_speed / acceleration_time)
 	elif direction and parent.velocity.x and directional_snap:
 		parent.velocity.x = - parent.velocity.x
@@ -148,25 +113,24 @@ func _handle_horizontal_movement(delta: float) -> void:
 
 
 func _handle_vertical_movement(delta: float) -> void:
-	if not parent.is_on_floor():
+	var on_floor := parent.is_on_floor()
+	
+	if not on_floor:
 		parent.velocity += parent.get_gravity() * gravity_scale * delta
 		parent.velocity.y = min(parent.velocity.y, terminal_falling_velocity)
 	
 	if Input.is_action_pressed(input_jump):
 		jump_buffering = jump_buffering_time
-	if parent.is_on_floor():
+	if on_floor:
 		coyote_jump = coyote_jump_time
-	if (jump_buffering > 0 and parent.is_on_floor()) or (coyote_jump > 0 and Input.is_action_just_pressed(input_jump)):
+	
+	if (jump_buffering > 0 and on_floor) or (coyote_jump > 0 and Input.is_action_just_pressed(input_jump)):
 			parent.velocity.y = - 2 * jump_height * jump_duration
 			jump_buffering = 0
 			coyote_jump = 0
 	elif Input.is_action_just_released(input_jump) and parent.velocity.y <= 0:
 		parent.velocity.y *= variable_jump_scale
 	
-	if jump_buffering > 0:
-		jump_buffering = max(0, jump_buffering - delta)
-	if coyote_jump > 0:
-		coyote_jump = max(0, coyote_jump - delta)
 
 
 func _try_corner_correction() -> void:
@@ -177,3 +141,55 @@ func _try_corner_correction() -> void:
 		parent.position.x += corner_correction_amount
 	elif not left_colliding and right_colliding:
 		parent.position.x -= corner_correction_amount
+
+
+func _setup_corner_correction_nodes() -> void:
+	_corner_correction_left = get_node_or_null(CC_LEFT_NAME)
+	_corner_correction_middle = get_node_or_null(CC_MIDDLE_NAME)
+	_corner_correction_right = get_node_or_null(CC_RIGHT_NAME)
+	
+	if not _corner_correction_left:
+		_corner_correction_left = _create_ray_cast(CC_LEFT_NAME)
+		add_child(_corner_correction_left)
+	if not _corner_correction_middle:
+		_corner_correction_middle = _create_shape_cast(CC_MIDDLE_NAME)
+		add_child(_corner_correction_middle)
+	if not _corner_correction_right:
+		_corner_correction_right = _create_ray_cast(CC_RIGHT_NAME)
+		add_child(_corner_correction_right)
+	
+	_update_corner_correction_properties()
+
+
+func _create_ray_cast(node_name: String) -> RayCast2D:
+	var raycast := RayCast2D.new()
+	raycast.name = node_name
+	raycast.collision_mask = parent.collision_mask
+	return raycast
+
+
+func _create_shape_cast(node_name: String) -> ShapeCast2D:
+	var shape_cast := ShapeCast2D.new()
+	shape_cast.name = node_name
+	shape_cast.shape = RectangleShape2D.new()
+	shape_cast.collision_mask = parent.collision_mask
+	return shape_cast
+
+
+func _update_corner_correction_properties() -> void:
+	if not (_corner_correction_left and _corner_correction_right and _corner_correction_middle):
+		return
+	
+	var half_distance := corner_correction_distance_between / 2
+	var rays: Array[RayCast2D] = [_corner_correction_left, _corner_correction_right]
+	for i in 2:
+		rays[i].position = corner_correction_offset
+		rays[i].position.x += half_distance * [-1, 1][i]
+		rays[i].target_position.y = -corner_correction_ray_length
+	
+	var middle := _corner_correction_middle
+	middle.position = corner_correction_offset
+	middle.position.y -= corner_correction_ray_length / 2
+	middle.target_position = Vector2.ZERO
+	middle.shape.size.x = maxf(0, corner_correction_distance_between - 2 * corner_correction_amount)
+	middle.shape.size.y = corner_correction_ray_length
