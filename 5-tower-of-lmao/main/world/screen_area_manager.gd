b7817c6c551fe4c53000ab37ff1a3593e790b128
leafcabral <rafaelcabral4100@gmail.com>
2026-02-02 20:28:38 -0300

    feat(tower_of_lmao): add screen transition between ScreenAreas

--- 5-tower-of-lmao/main/world/screen_area_manager.gd
+++ 5-tower-of-lmao/main/world/screen_area_manager.gd
@@ -3,6 +3,9 @@ extends Node
 
 
 @export var player_camera: Camera2D
+@export_range(0, 1, 0.01, "or_greater") var transition := 0.3
+
+var current_screen: ScreenArea
 
 
 func _ready() -> void:
@@ -12,7 +15,12 @@ func _ready() -> void:
 
 func _on_screen_area_player_entered(area: ScreenArea) -> void:
 	var screen_rect := area.get_rect()
-	player_camera.limit_top = screen_rect.position.y
-	player_camera.limit_left = screen_rect.position.x
-	player_camera.limit_bottom = screen_rect.end.y
-	player_camera.limit_right = screen_rect.end.x
+	
+	var time := transition if current_screen else 0.0
+	var tween := player_camera.create_tween()
+	tween.tween_property(player_camera, "limit_top", screen_rect.position.y, time)
+	tween.parallel().tween_property(player_camera, "limit_left", screen_rect.position.x, time)
+	tween.parallel().tween_property(player_camera, "limit_bottom", screen_rect.end.y, time)
+	tween.parallel().tween_property(player_camera, "limit_right", screen_rect.end.x, time)
+	
+	current_screen = area
