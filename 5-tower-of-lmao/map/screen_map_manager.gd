15f717a790be7cc09169880a8ede67bb9ab53852
leafcabral <rafaelcabral4100@gmail.com>
2026-02-05 22:00:46 -0300

    fix(tower_of_lmao): fix screen not moving when player is between screens
    The previous behavior was caused due to ScreenMapManager not handling the player leaving screen.
    
    That made the screen always stuck to the last screen entered when the player is between two or more screens.

--- 5-tower-of-lmao/map/screen_map_manager.gd
+++ 5-tower-of-lmao/map/screen_map_manager.gd
@@ -4,20 +4,36 @@ extends Node2D
 
 @export var player_camera: Camera2D
 
+var screens: Array[ScreenMap]
 var current_screen: ScreenMap
 
 
 func _ready() -> void:
-	for i in find_children("*", "ScreenMap"):
+	var all_screens := find_children("*", "ScreenMap")
+	for i in all_screens:
 		i.player_entered.connect(_on_screen_area_player_entered)
+		i.player_left.connect(_on_screen_map_player_left)
+
+
+func _handle_screen_change() -> void:
+	var last_entered_screen := screens[-1]
+	
+	if current_screen != last_entered_screen:
+		var screen_rect := last_entered_screen.get_global_rect()
+		
+		player_camera.limit_top = screen_rect.position.y
+		player_camera.limit_left = screen_rect.position.x
+		player_camera.limit_bottom = screen_rect.end.y
+		player_camera.limit_right = screen_rect.end.x
+		
+		current_screen = last_entered_screen
 
 
 func _on_screen_area_player_entered(area: ScreenMap) -> void:
-	var screen_rect := area.get_global_rect()
-	
-	player_camera.limit_top = screen_rect.position.y
-	player_camera.limit_left = screen_rect.position.x
-	player_camera.limit_bottom = screen_rect.end.y
-	player_camera.limit_right = screen_rect.end.x
-	
-	current_screen = area
+	screens.append(area)
+	_handle_screen_change()
+
+
+func _on_screen_map_player_left(area: ScreenMap) -> void:
+	screens.erase(area)
+	_handle_screen_change()
