2ad5c943b6b1fef2a72d933f68f3e78b72bcfcfc
leafcabral <rafaelcabral4100@gmail.com>
2026-02-15 10:44:40 -0300

    feat(tower_of_lmao): add sprites to checkpoints

--- 5-tower-of-lmao/map/checkpoint.gd
+++ 5-tower-of-lmao/map/checkpoint.gd
@@ -1,45 +1,60 @@
 @tool
+@icon("res://map/checkpoint_icon.png")
 class_name Checkpoint
 extends Area2D
 
 
 signal player_entered(checkpoint: Checkpoint)
 
-const COLLISION_SHAPE_NAME := "CheckpointArea"
+@export var area: RectangleShape2D:
+	set(value):
+		area = value
+		update_checkpoin_area()
+@export var offset := Vector2i.ZERO:
+	set(value):
+		offset = value
+		update_checkpoin_area()
+@export var activated := false:
+	set(value):
+		activated = value
+		var fire := $Fire
+		if activated:
+			fire.show()
+			fire.play("default")
+			await fire.animation_finished
+			fire.play("active")
+		else:
+			fire.hide()
 
-var collision_shape: CollisionShape2D
-var shape: RectangleShape2D
+var checkpoint_area: CollisionShape2D
 
 
 func _ready() -> void:
-	_create_collision_shape()
-	
+	checkpoint_area = get_node_or_null("CheckpointArea")
+	area = RectangleShape2D.new()
 	body_entered.connect(_on_body_entered)
 
 
+func get_shape() -> RectangleShape2D:
+	return $CheckpointArea.shape
+
+
 func get_center() -> Vector2:
-	return to_global(shape.get_rect().get_center())
+	return to_global(get_shape().get_rect().get_center())
 
 
 func get_base() -> Vector2:
-	return to_global(Vector2(get_center().x, shape.get_rect().end.y))
+	return to_global(Vector2(get_center().x, get_shape().get_rect().end.y))
 
 
-func _create_collision_shape() -> void:
-	collision_shape = get_node_or_null(COLLISION_SHAPE_NAME)
-	
-	if not collision_shape:
-		collision_shape = CollisionShape2D.new()
-		collision_shape.name = COLLISION_SHAPE_NAME
-		shape = RectangleShape2D.new()
-		collision_shape.shape = shape
-		
-		add_child(collision_shape)
-		collision_shape.owner = get_tree().edited_scene_root
-	else:
-		shape = collision_shape.shape
+func update_checkpoin_area() -> void:
+	if checkpoint_area:
+		checkpoint_area.shape = area
+		checkpoint_area.position = offset
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		player_entered.emit(self)
+		if not activated:
+			player_entered.emit(self)
+			activated = true
