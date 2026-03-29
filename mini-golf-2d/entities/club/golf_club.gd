e8b529e3f979ff52d45b451bd7f7dbe2f33944e0
leafcabral <rafaelcabral4100@gmail.com>
2026-03-29 12:51:17 -0300

    feat: add club swing animation
    Improve club visuals and its code readabilty

--- mini-golf-2d/entities/club/golf_club.gd
+++ mini-golf-2d/entities/club/golf_club.gd
@@ -12,41 +12,49 @@ signal swing_ended
 @onready var shot_area: DragThrowArea2D = $ShotArea
 @onready var club: Sprite2D = $Club
 @onready var swing_direction: Sprite2D = $SwingDirection
-@onready var club_offset_x := club.offset.x
+@onready var CLUB_OFFSET_X := absf(club.offset.x)
 
 var is_swinging := false
 
 
 func _physics_process(_delta: float) -> void:
-	if Engine.is_editor_hint():
+	if not is_swinging or Engine.is_editor_hint():
 		return
 	
-	if is_swinging:	
-		var magnitude := shot_area.get_drag_baked_length()
-		swing_direction.rotation = shot_area.drag.angle()
-		swing_direction.scale.x = remap(magnitude, 0, 1, 1, 5)
-		club.offset.x = club_offset_x * remap(magnitude, 0, 1, 0.7, 2)
-		if shot_area.should_invert():
-			club.offset.x *= -1
-			club.flip_h = true
-		else:
-			club.flip_h = false
-		
-		swing_direction.visible = true if shot_area.drag else false
+	update_visuals()
+
+
+func update_visuals() -> void:
+	swing_direction.visible = shot_area.drag != Vector2.ZERO
+	club.visible = swing_direction.visible
+	if not swing_direction.visible:
+		return
+	
+	var magnitude := shot_area.get_drag_baked_length()
+	
+	swing_direction.scale.x = remap(magnitude, 0, 1, 1, 5)
+	swing_direction.rotation = shot_area.drag.angle()
+	
+	club.position.x = remap(magnitude, 0, 1, 0.3, 2) * 50
+	club.offset.x = CLUB_OFFSET_X
+	club.flip_h = shot_area.should_invert()
+	if not club.flip_h:
+		club.offset.x *= -1
+		club.position.x *= -1
 
 
 func _on_shot_area_grabbed() -> void:
 	is_swinging = true
 	swing_started.emit()
-	
-	club.visible = true
 
 
 func _on_shot_area_released(cancelled: bool) -> void:
 	is_swinging = false
 	swing_ended.emit()
-	if ball and not cancelled:
-		ball.apply_central_impulse(shot_area.drag * strength)
 	
-	club.visible = false
 	swing_direction.visible = false
+	var club_tween := club.create_tween()
+	club_tween.tween_property(club, "position:x", 0, 0.05)
+	if ball and not cancelled:
+		club_tween.tween_callback(ball.apply_central_impulse.bind(shot_area.drag * strength))
+	club_tween.tween_property(club, "visible", false, 0.1)
