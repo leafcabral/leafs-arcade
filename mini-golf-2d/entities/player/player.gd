39479e24627b8752e61c4998a7e5f9e645a1ab61
leafcabral <rafaelcabral4100@gmail.com>
2026-04-12 09:52:57 -0300

    feat: add camera zoom in and out

--- mini-golf-2d/entities/player/player.gd
+++ mini-golf-2d/entities/player/player.gd
@@ -5,6 +5,8 @@ extends Node
 const DRIVER_CLUB := preload("uid://co7lprfyeuwgs")
 const WOOD_CLUB := preload("uid://cllyoka27wb4x")
 const WEDGE_CLUB := preload("uid://dqisdeotk3fkt")
+const MAX_ZOOM := Vector2.ONE * 1.5
+const MIN_ZOOM := Vector2.ONE * 0.25
 
 @export var pos_reset_start := Vector2.ZERO
 
@@ -14,6 +16,7 @@ var pos_reset_last := Vector2.ZERO
 
 @onready var club: GolfClub = $Club
 @onready var ball: GolfBall = $Ball
+@onready var camera: Camera2D = $Ball/Camera
 
 
 func _ready() -> void:
@@ -34,6 +37,30 @@ func _input(event: InputEvent) -> void:
 		ball.teleport(pos_reset_last)
 	if event.is_action_pressed("reset_start"):
 		ball.teleport(pos_reset_start)
+	
+	if event.is_action_pressed("zoom_reset"):
+		camera.zoom = Vector2.ONE
+
+
+func _process(delta: float) -> void:
+	var zoom_amount := Input.get_axis("zoom_out", "zoom_in") * delta
+	if zoom_amount:
+		zoom(zoom_amount)
+	else:
+		# Mouse scroll only activates is_action_just_pressed
+		zoom_amount = delta * 3
+		if Input.is_action_just_pressed("zoom_in"):
+			zoom(zoom_amount)
+		elif Input.is_action_just_pressed("zoom_out"):
+			zoom(-zoom_amount)
+
+
+func zoom(amount: float)-> void:
+	var new_zoom := camera.zoom + Vector2(amount, amount)
+	if amount > 0:
+		camera.zoom = MAX_ZOOM.min(new_zoom)
+	elif amount < 0:
+		camera.zoom = MIN_ZOOM.max(new_zoom)
 
 
 func get_hud_data() -> Dictionary:
